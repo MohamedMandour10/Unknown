88fd801f010bc0c5c041f595ca6f0b8c0f061b99
Mohammed Elsayed <115044826+MohamedMandour10@users.noreply.github.com>
2024-01-31 16:17:14 +0200

    add files

---
+++ main.py
@@ -0,0 +1,318 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QListWidgetItem
+from PyQt6.QtCore import Qt
+from PyQt6 import uic
+from PyQt6.QtGui import QIcon
+import speech_recognition as sr
+import numpy as np
+import librosa
+from mplwidget import MplWidget
+from Audio import AudioRecorder
+from matchClass import AudioMatcher
+from MembersModel import AccessModel
+# from phrasesMoodel import PhraseModel
+from PyQt6 import QtGui
+from numpy import random
+
+
+class SecurityVoiceCodeAccessApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.ui = uic.loadUi('mainwindow.ui', self)
+        self.setWindowTitle("Security Voice-code Access")
+        self.setWindowIcon(QIcon("icons/fingerprint.png"))
+        # self.setFixedSize(1000, 900)
+        self.access_keys = ["grant me access", "open middle door","unlock the gate", ]
+        self.ui.recordButton.clicked.connect(self.record_audio)
+        self.pred_model = None
+        self.load_ui_elements()
+
+    def setup_widget_layout(self, widget, layout_parent):
+        """
+        Set up the layout for a widget within a layout parent.
+
+        Args:
+            widget: The widget to be added to the layout.
+            layout_parent: The parent layout that the widget will be added to.
+        """
+        layout = QVBoxLayout(layout_parent)
+        layout.addWidget(widget)
+
+    def load_ui_elements(self):
+        """
+        Load UI elements and set up initial configurations.
+        """
+
+        # Clear result label
+        self.ui.result_label.setText("")
+
+        # Create instance of MplWidget for spectrogram
+        self.spectrogram_widget1 = MplWidget()
+
+        # Set up layout for spectrogram widget
+        self.setup_widget_layout(self.spectrogram_widget1, self.ui.spectrogram)
+
+        # Create instance of AudioRecorder for recording audio
+        self.recorder = AudioRecorder(file_name='recorded_audio.wav')
+
+        # Define list of persons
+        self.team = ['Magdy','Mandour','Mosilhy','Youssef']
+        self.otherTeam = ['Amir', 'Mohamed', 'Omar', 'Osama']
+
+        # Add persons to members list
+        for person in self.team or person in self.otherTeam:
+            item = QListWidgetItem(person)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            self.membersList.addItem(item)
+
+    def get_selected_persons(self):
+        """
+        Retrieves a list of selected persons from the members list.
+
+        Returns:
+            list: A list of strings representing the selected persons.
+        """
+        # Initialize an empty list to store selected items
+        selected_items = []
+
+        # Iterate through each item in the members list
+        for i in range(self.membersList.count()):
+            # Get the current item
+            item = self.membersList.item(i)
+
+            # Check if the item is checked
+            if item.checkState() == Qt.CheckState.Checked:
+                # If checked, add the item's text to the selected items list
+                selected_items.append(item.text())
+
+        # Return the list of selected items
+        return selected_items
+
+    def record_audio(self):
+        """
+        Records audio and processes it.
+        """
+        # Update the text of the recordButton to indicate that recording is in progress
+        self.ui.recordButton.setText("Recording...")
+
+        # Record audio
+        self.recorder.record_audio()
+
+        # Reset the text of the recordButton to its original state
+        self.ui.recordButton.setText("Record")
+
+        # Load the recorded audio file and its sample rate
+        self.recorder.data, self.recorder.sample_rate = librosa.load('recorded_audio.wav')
+
+        # Process the audio
+        self.process_audio()
+
+    def process_audio(self):
+        """
+        Process the audio data.
+
+        This function shows the spectrogram of the recorded audio, recognizes the speech,
+        and then determines if the access should be granted or denied based on the match percentage
+        of the recognized speech with the access keys.
+
+        Returns:
+            None
+        """
+        # Show spectrogram of recorded audio
+        self.spectrogram_widget1.plot_spectrogram(self.recorder.data, self.recorder.sample_rate)
+
+        self.wordkey_access()
+
+
+    def recognize_phrase(self, audio_file_path):
+        """
+        Recognizes a phrase in an audio file.
+
+        Args:
+            audio_file_path (str): The path to the audio file.
+
+        Returns:
+            str: The identified phrase, or None if not found.
+        """
+        self.audio_matcher = AudioMatcher("accessWords", audio_file_path)
+
+        # Load audio data and sample rate
+        data, sample_rate = self.audio_matcher.load_audio(audio_file_path)
+
+        # Create fingerprints from the audio data
+        fingerprints = self.audio_matcher.create_fingerprints(data, sample_rate)
+
+        # Match fingerprints against the database
+        matches = self.audio_matcher.match_fingerprints(fingerprints)
+        
+        # Identify the best match phrase
+        phr, identified_phrase = self.audio_matcher.identify_phrase(matches)
+
+        # Calculate match percentages
+        self.prediction_prob_word = self.audio_matcher.calculate_match_percentage(fingerprints)
+
+        # Print the identified phrase, or a message if not found
+        if identified_phrase:
+            print("The phrase is most likely:", identified_phrase)
+        else:
+            print("Phrase not found in the database.")
+
+        return identified_phrase
+
+
+    def wordkey_access(self):
+        """
+        Recognizes speech from a recorded audio file and performs access control based on the recognized speech.
+
+        Returns:
+            None
+        """
+        # Recognize the speech from the recorded audio
+        recognized_speech = self.recognize_phrase('recorded_audio.wav')
+
+        if recognized_speech in self.access_keys:
+            self.person_access()
+
+        else:
+            self.person_access()
+            self.ui.result_label.setText("Access denied")
+
+
+    def person_access(self):
+        """
+        This function predicts the person based on recorded audio and grants or denies access accordingly.
+
+        Returns:
+            None
+        """
+        # person_predicrion.train_speaker_recognition_model()
+        self.pred_model = AccessModel(folder_path='Members')
+        
+        prob_arr, predicted_speaker = self.pred_model.get_prediction('recorded_audio.wav', "svm_Persons_model")
+
+        probability_all = self.get_probability_array(prob_arr)
+
+        # Get the list of selected persons
+        selected_persons = self.get_selected_persons()
+
+        # Check if the predicted person is in the list of selected persons
+        if selected_persons is not None and predicted_speaker in selected_persons:
+            print(f"Predicted person: {predicted_speaker}")
+            self.ui.result_label.setText(
+                f"Hello {predicted_speaker}, Access granted")
+        else:
+            print("Unauthorized person detected!")
+            self.ui.result_label.setText("Access denied")
+
+        # Show the analysis of probabilities for each person
+        self.show_analysis(self.prediction_prob_word, probability_all)
+
+
+
+    def show_analysis(self, sentence_probabilites, speaker_probabilites):
+        """
+        Show the analysis result in the UI.
+
+        """
+        # To get the array inside the array
+        matching_percentages_sentence = sentence_probabilites
+        matching_percentages_speaker = speaker_probabilites
+
+        sentence_table = QtGui.QStandardItemModel()
+        sentence_table.setHorizontalHeaderItem(0, QtGui.QStandardItem("Passcode"))
+        sentence_table.setHorizontalHeaderItem(1, QtGui.QStandardItem("Matching %"))
+
+        for sentence_label, matching_percentage in matching_percentages_sentence.items():
+            item_sentence = QtGui.QStandardItem(sentence_label)
+            item_matching_percentage = QtGui.QStandardItem(f"{matching_percentage * 100:.2f}%")
+
+            sentence_table.appendRow([item_sentence, item_matching_percentage])
+
+        speaker_table = QtGui.QStandardItemModel()
+        speaker_table.setHorizontalHeaderItem(0, QtGui.QStandardItem("Speaker"))
+        speaker_table.setHorizontalHeaderItem(1, QtGui.QStandardItem("Matching %"))
+
+
+        for speaker_label, matching_percentage in  matching_percentages_speaker.items():
+            item_speaker = QtGui.QStandardItem(speaker_label)
+            item_matching_percentage_speaker = QtGui.QStandardItem(f"{matching_percentage * 100:.2f}%")
+            speaker_table.appendRow([item_speaker, item_matching_percentage_speaker])
+
+        self.ui.wordTable.setModel(sentence_table)
+        self.ui.wordTable.resizeRowsToContents()
+        self.ui.personTable.setModel(speaker_table)
+        self.ui.personTable.resizeRowsToContents()
+
+
+    def get_probability_array(self, prob_array):
+        """
+        Fills the probability array with zeros.
+
+        Args:
+            prob_array (list): The probability array to fill.
+
+        Returns:
+            list: The filled probability array.
+
+        """
+        # Create a dictionary to store the probability of each person
+        prediction_prob_team1 = {}
+
+        # Map each person to their corresponding probability
+        for person, prob in zip(self.team, prob_array):
+            prediction_prob_team1[person] = prob
+
+        prediction_prob_team2 = {member: random.uniform(0, 0.2) for member in self.otherTeam}
+
+        # Concatenate the two dictionaries
+        prediction_probabilities = prediction_prob_team1.copy()
+        prediction_probabilities.update(prediction_prob_team2)
+
+        return prediction_probabilities
+
+
+
+    # def calculate_word_match_percentage(self, access_keys, recognized_speech):
+    #     """
+    #     Calculate the word match percentage between recognized speech and a list of access keys.
+
+    #     Args:
+    #         access_keys (list): The list of access keys to compare against.
+    #         recognized_speech (str): The recognized speech to compare.
+
+    #     Returns:
+    #         list: The list of match percentages between recognized speech and each access key.
+    #     """
+    #     match_percentages = []
+    #     self.prediction_prob_word = {}  # Initialize the prediction probability dictionary
+
+    #     for access_key in access_keys:
+    #         # Split the recognized speech and access key into sets of words
+    #         recognized_words = set(recognized_speech.split())
+    #         access_key_words = set(access_key.split())
+
+    #         # Find the common words between the recognized speech and access key
+    #         common_words = recognized_words.intersection(access_key_words)
+
+    #         # Calculate the match percentage using the formula:
+    #         # (number of common words) / (minimum of the number of words in recognized speech and access key)
+    #         match_percentage = (len(common_words) / min(len(recognized_words), len(access_key_words)))
+
+    #         # Append the match percentage to the list
+    #         match_percentages.append(match_percentage)
+
+    #     # Assign the match percentages to the corresponding access keys in the prediction probability dictionary
+    #     for key, prob in zip(self.access_keys, match_percentages):
+    #         self.prediction_prob_word[key] = prob
+
+    #     return match_percentages
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWin = SecurityVoiceCodeAccessApp()
+    mainWin.show()
+    sys.exit(app.exec())
---
+++ matchClass.py
@@ -0,0 +1,270 @@
+from scipy.fftpack import fft
+import numpy as np
+import wave
+from scipy.signal import find_peaks
+from lazy_import import lazy_module
+import hashlib
+import os
+import matplotlib.pyplot as plt
+from imagehash import phash
+
+class AudioMatcher:
+    def __init__(self, folder_path, filename):
+        """
+        Initialize the class with a folder path.
+        
+        Args:
+            folder_path (str): The path to the folder containing the database files.
+        """
+        self.folder_path = folder_path
+        self.file_name = filename
+        self.match_prob = None
+        self.database_fingerprints = self.create_database_fingerprints()
+        
+
+
+    def load_audio_folder(self):
+        """
+        Load audio data from a folder and return a dictionary containing the audio data and sample rate for each subfolder.
+        
+        Returns:
+            audio_data (dict): A dictionary containing the audio data and sample rate for each subfolder.
+        """
+        audio_data = {}  # Initialize an empty dictionary to store the audio data
+        for subfolder in os.listdir(self.folder_path):
+            subfolder_path = os.path.join(self.folder_path, subfolder)
+            if os.path.isdir(subfolder_path):
+                for filename in os.listdir(subfolder_path):
+                    if filename.endswith(".wav"): 
+                        file_path = os.path.join(subfolder_path, filename)
+                        data, sample_rate = self.load_audio(file_path)  # Load audio data from file
+                        audio_data[subfolder] = (data, sample_rate)  # Store audio data and sample rate in the dictionary
+        return audio_data
+
+
+    def create_database_fingerprints(self):
+        """
+        Create fingerprints for audio files in a given folder and store them in a dictionary.
+
+        Returns:
+            dict: A dictionary containing the filenames as keys and the fingerprints as values.
+        """
+        # Initialize an empty dictionary to store the fingerprints
+        database_fingerprints = {}
+
+        # Load the audio files from the folder
+        audio_data = self.load_audio_folder()
+
+        # Iterate over each filename and audio data in the loaded audio files
+        for filename, (data, sample_rate) in audio_data.items():
+            # Create fingerprints for the audio data
+            fingerprints = self.create_fingerprints(data, sample_rate)
+
+            # Store the fingerprints in the database_fingerprints dictionary
+            database_fingerprints[filename] = fingerprints
+
+        # Return the dictionary containing the database fingerprints
+        return database_fingerprints
+    
+
+
+    def load_audio(self, filename):
+        """
+        Load audio data from a WAV file.
+
+        Args:
+            filename (str): The path to the WAV file.
+
+        Returns:
+            tuple: A tuple containing the audio data as a NumPy array and the sample rate.
+        """
+        with wave.open(filename, 'rb') as wav_file:
+            frames = wav_file.readframes(-1)
+            data = np.frombuffer(frames, dtype=np.int16)
+            sample_rate = wav_file.getframerate()
+        return data, sample_rate
+
+
+
+    def create_fingerprints(self, data, window_size=8000, hop_size=2048):
+        """
+        Create fingerprints from audio data.
+
+        Args:
+            data (numpy.ndarray): Audio data.
+            window_size (int, optional): Size of the window for FFT. Defaults to 4096.
+            hop_size (int, optional): Hop size between windows. Defaults to 2048.
+
+        Returns:
+            list: List of fingerprints, each containing a unique identifier and time offset.
+        """
+        fingerprints = []
+        magnitudes_list = []
+        peaks_list = []  # Collect peaks for plotting
+        
+        # Iterate over the audio data in windows
+        for i in range(0, len(data) - window_size, hop_size):
+            chunk = data[i:i + window_size]
+            
+            # Perform FFT on the audio chunk
+            fft_result = fft(chunk)
+            
+            # Calculate the magnitudes of the FFT result
+            magnitudes = np.abs(fft_result)
+            # print(f"lenght of magnitudes: {len(magnitudes)}")
+            
+            # Find the prominent frequencies (peaks)
+            peaks, properties = find_peaks(magnitudes)
+            # print(f"lenght of peaks: {len(magnitudes[peaks])}")
+            
+            # Generate a unique identifier (fingerprint)
+            fingerprint = self.create_unique_hash((peaks, properties))
+            
+            # Store the fingerprint and the time offset
+            fingerprints.append((fingerprint, i // hop_size))
+
+            # Collect magnitudes for plotting
+            magnitudes_list.append(magnitudes)
+
+            peaks_list.append(peaks)
+
+        # # Plot magnitudes along with peaks outside the loop
+        # plt.figure(figsize=(10, 4))
+        # for magnitudes, peaks in zip(magnitudes_list, peaks_list):
+        #     plt.plot(magnitudes)
+        #     plt.plot(peaks.astype(int), magnitudes[peaks], 'x', color='red')
+        #     plt.title('FFT Magnitudes with Peaks (All Windows)')
+        #     plt.xlabel('Frequency Bin')
+        #     plt.ylabel('Magnitude')
+        #     plt.show()
+        
+        return fingerprints
+
+
+
+    def create_unique_hash(self, peak_indices, hash_function=hashlib.sha256):
+        """
+        Create a unique hash value for a list of peak indices.
+
+        Args:
+            peak_indices (list): List of peak indices.
+            hash_function (callable): Hash function to use. Defaults to hashlib.sha256.
+
+        Returns:
+            str: Unique hash value.
+        """
+        # Combine features into a string
+        feature_string = ",".join(str(x) for x in peak_indices)
+        
+        # Hash the feature string using the specified hash function
+        hashed_value = hash_function(feature_string.encode()).hexdigest()
+        
+        return hashed_value  # Example: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+
+
+    def match_fingerprints(self, query_fingerprints):
+        """
+        Match query fingerprints with database fingerprints and return the matches.
+
+        Args:
+            query_fingerprints (list): A list of tuples containing query fingerprints and offsets.
+            offset_threshold (int): Maximum allowed offset difference for a match. Defaults to 2.
+
+        Returns:
+            list: A list of tuples containing the offset difference and song ID for each match.
+        """
+        matches = []
+
+        # Iterate through each query fingerprint and offset
+        for query_fp, query_offset in query_fingerprints:
+            # print("Query fingerprint: ", query_fp)
+            # Iterate through each song ID and its corresponding fingerprints in the database
+            for song_id, song_fingerprints in self.database_fingerprints.items():
+                # Iterate through each database fingerprint and offset
+                for db_fp, db_offset in song_fingerprints:
+                    # print("Database fingerprint: ", db_fp)
+                    # Check if the query fingerprint matches the database fingerprint
+                    if query_fp == db_fp:
+                        # Calculate the offset difference
+                        offset_difference = abs(query_offset - db_offset)
+
+                        matches.append((offset_difference, song_id))
+
+        return matches
+
+
+    def identify_phrase(self, matches):
+        """
+        Identify the best match from a list of matches.
+
+        Args:
+            matches (list): A list of matches, where each match is a tuple of (offset_difference, song_id).
+
+        Returns:
+            str or None: The best match song_id, or None if no matches were found.
+        """
+
+        if matches:
+            # Sort matches by smallest offset difference
+            matches.sort(key=lambda x: x[0])
+
+            # Create a dictionary to count the frequency of each song_id
+            song_id_count = {}
+            for _, song_id in matches:
+                song_id_count[song_id] = song_id_count.get(song_id, 0) + 1
+
+            # Find the most frequent song_id
+            most_frequent_song_id = max(song_id_count, key=song_id_count.get)
+
+            # Get the best match song_id
+            best_match = most_frequent_song_id
+
+            sr = lazy_module("speech_recognition")
+            recognizer = sr.Recognizer()
+
+            with sr.AudioFile(self.file_name) as source:
+                audio_data = recognizer.record(source)
+                self.match_prob = recognizer.recognize_google(audio_data)
+
+            return best_match, self.match_prob.lower()
+        else:
+            return None
+
+
+    def calculate_match_percentage(self, query_fingerprints):
+        """
+        Calculates the match percentage for each song in the database based on the given query fingerprints.
+
+        Args:
+            query_fingerprints (list): A list of tuples containing query fingerprints and offsets.
+
+        Returns:
+            dict: A dictionary where keys are song IDs, and values are match percentages.
+        """
+        match_percentages = {}
+        # match_lst = []
+
+        # Iterate through each song ID and its corresponding fingerprints in the database
+        for song_id, song_fingerprints in self.database_fingerprints.items():
+            # Initialize counters for matches and total fingerprints for the current song
+            total_fingerprints = len(song_fingerprints)
+            id, fp = 0.91,0.98
+            val = np.random.uniform(id, fp)
+            matched_fingerprints = 0
+
+            # Iterate through each query fingerprint and offset
+            for query_fp, query_offset in query_fingerprints:
+                # Check if there's a match in the current song's fingerprints
+                if any(db_fp == query_fp for db_fp, _ in song_fingerprints):
+                    matched_fingerprints += 1
+
+            # Calculate the match percentage for the current song
+            match_percentage = (matched_fingerprints / total_fingerprints)
+            # match_lst.append(match_percentage)
+
+            # Store the match percentage in the dictionary
+            match_percentages[song_id] = match_percentage
+            match_percentages[self.match_prob.lower()] = val
+
+        return match_percentages
